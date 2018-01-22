6986f4820aaf353384674cf165285fde5e1a5e3d
Yoonku Hwang <yz0624@gmail.com>
2018-01-22 15:23:13 +0900

    feed forward

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -20,8 +20,7 @@ class NeuralNetwork(object):
         #
         # Note: in Python, you can define a function with a lambda expression,
         # as shown below.
-        self.activation_function = lambda x : 0  # Replace 0 with your sigmoid calculation.
-
+        self.activation_function = lambda x : 1. / (1. + np.exp(-x))  # Replace 0 with your sigmoid calculation.
         ### If the lambda code above is not something you're familiar with,
         # You can uncomment out the following three lines and put your
         # implementation there instead.
@@ -64,12 +63,14 @@ class NeuralNetwork(object):
         #### Implement the forward pass here ####
         ### Forward pass ###
         # TODO: Hidden layer - Replace these values with your calculations.
-        hidden_inputs = None # signals into hidden layer
-        hidden_outputs = None # signals from hidden layer
+        weights_input2hidden = self.weights_input_to_hidden
+        hidden_inputs = X # signals into hidden layer
+        hidden_outputs = self.activation_function(np.dot(hidden_inputs, weights_input2hidden)) # signals from hidden layer
 
         # TODO: Output layer - Replace these values with your calculations.
-        final_inputs = None # signals into final output layer
-        final_outputs = None # signals from final output layer
+        weights_hidden2output = self.weights_hidden_to_output
+        final_inputs = hidden_outputs # signals into final output layer
+        final_outputs = np.dot(hidden_outputs, weights_hidden2output) # signals from final output layer
 
         return final_outputs, hidden_outputs
 
@@ -88,7 +89,7 @@ class NeuralNetwork(object):
         ### Backward pass ###
 
         # TODO: Output error - Replace this value with your calculations.
-        error = None # Output layer error is the difference between desired target and actual output.
+        error = y - final_outputs # Output layer error is the difference between desired target and actual output.
 
         # TODO: Calculate the hidden layer's contribution to the error
         hidden_error = None
